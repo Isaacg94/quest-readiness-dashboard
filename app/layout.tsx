51079a2d4cd045f893142797b87fbe5113b5f84e
Isaacg94 <7248zack@gmail.com>
2026-02-12 17:03:08 +0300

    Minor metadata udates

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: {
-    default: "Quest – Your Future Awaits",
+    default: "Quest | Your Future Awaits",
     template: "%s | Quest",
   },
   description:
